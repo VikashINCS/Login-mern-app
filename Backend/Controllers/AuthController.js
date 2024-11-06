3f2876ba9d1ff2bd49df3805e0ca02f66d487466
VikashINCS <156886399+VikashINCS@users.noreply.github.com>
2024-11-06 09:35:41 +0530

    Update AuthController.js

--- Backend/Controllers/AuthController.js
+++ Backend/Controllers/AuthController.js
@@ -1,1 +1,98 @@
+const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
+const UserModel = require("../Models/user");
+
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(cors({
+  origin: "https://login-mern-app.vercel.app", // Your frontend URL
+  methods: "GET, POST, PUT, DELETE, OPTIONS",
+  allowedHeaders: "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+}));
+
+app.use(express.json());
+
+const signup = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+    const user = await UserModel.findOne({ email });
+    if (user) {
+      return res.status(409).json({
+        message: "User already exists, please login.",
+        success: false,
+      });
+    }
+    const userModel = new UserModel({
+      name,
+      email,
+      password,
+    });
+
+    userModel.password = await bcrypt.hash(password, 10);
+    await userModel.save();
+    res.status(201).json({
+      message: "Signup successful.",
+      success: true,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Internal server error.",
+      success: false,
+    });
+  }
+};
+
+const login = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const user = await UserModel.findOne({ email });
+    const errorMessage = "Authentication failed. Email or password is incorrect.";
+    if (!user) {
+      return res.status(403).json({
+        message: errorMessage,
+        success: false,
+      });
+    }
+    const isPasswordValid = await bcrypt.compare(password, user.password);
+    if (!isPasswordValid) {
+      return res.status(403).json({
+        message: errorMessage,
+        success: false,
+      });
+    }
+    const jwtToken = jwt.sign(
+      { email: user.email, _id: user._id },
+      process.env.JWT_SECRET,
+      { expiresIn: "24h" }
+    );
+
+    res.status(201).json({
+      message: "Login successful.",
+      success: true,
+      jwtToken,
+      email,
+      name: user.name,
+    });
+  } catch (err) {
+    res.status(500).json({
+      message: "Internal server error.",
+      success: false,
+    });
+  }
+};
+
+app.post('/auth/signup', signup);
+app.post('/auth/login', login);
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+module.exports = {
+  signup,
+  login,
+};
 
