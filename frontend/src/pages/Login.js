14028eef90b002b60af5829bef986feb11a67c5a
VikashINCS <156886399+VikashINCS@users.noreply.github.com>
2024-11-06 09:25:54 +0530

    Update Login.js

--- frontend/src/pages/Login.js
+++ frontend/src/pages/Login.js
@@ -26,7 +26,7 @@ function Login() {
       return handleError("email,password are required..");
     }
     try {
-      const url = "http://localhost:8080/auth/login";
+      const url = "https://login-mern-app-api.vercel.app/auth/login";
       const response = await fetch(url, {
         method: "POST",
         headers: {
